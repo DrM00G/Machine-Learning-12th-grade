250c3bc796ac691c183bb1e67c1445446b85ce02
Dr. Moo <68446064+DrM00G@users.noreply.github.com>
2022-03-11 21:34:49 +0000

    idk

--- tiktac.py
+++ tiktac.py
@@ -1,4 +1,7 @@
 import random
+import matplotlib.pyplot as plt
+# import numpy as np
+
 class TikTacToe:
   def __init__(self,strat_num):
     strat_num=strat_num
@@ -9,12 +12,12 @@ class TikTacToe:
       possible_states.append(state)
     # print(len(possible_states))
 
-    str_states=[self.make_str(state) for state in possible_states]
-    step={state:0 for state in str_states}
+    # str_states=[self.make_str(state) for state in possible_states]
+    step={self.make_str(state):0 for state in possible_states}
     reduced_states = [self.make_list(state) for state in step.keys()]
+    str_states=[self.make_str(state) for state in reduced_states]
     # reduced_states = [self.make_list(state) for state in str_states]
     for n in range(strat_num):
-      print(n)
       strats.append({state:0 for state in str_states}) #here
       for i in range(len(reduced_states)):
         possible_choices=[]
@@ -27,7 +30,57 @@ class TikTacToe:
     # print(strats[0]["000000000"])
     # print(self.match_up(['0','0','0','0','0','0','0','0','0'], strats[0], strats[1]))
     print("start tourny")
+    original_gen=[]
+    previous_gen=[]
+    origin_comparison=[]
+    previous_comparison=[]
+    for strat in strats:
+      previous_gen.append(strat)
+      original_gen.append(strat)
+    for n in range(20):
+      strats=self.next_gen(strats)
+      previous_comparison.append(self.compare_gens(strats[0:5],previous_gen))
+      origin_comparison.append(self.compare_gens(strats[0:5],original_gen))
+      previous_gen=[]
+      for strat in strats:
+        previous_gen.append(strat)
+      
+    # plt.subplots()
+    fig, ax = plt.subplots()
+    ax.plot([n for n in range(20)], origin_comparison, linewidth=2.0)
+    
+
+    # plt.plot(origin_comparison, range(20))
+    plt.savefig('tikbestvspevious.png')
+      # for strat in strats:
+      #   print("------------")
+      #   for n in range(5):
+      #     print(strat[str_states[n]])
+    print("done did")
+    best_child=strats[0]
+
+    
+  def compare_gens(self,group_1,group_2):
+    score=[0 for n in range(len(group_1))]
+    for n in range(len(group_1)):
+      for strat in group_2:
+        outcome_1=self.check_win(self.match_up(['0','0','0','0','0','0','0','0','0'], group_1[n], strat))
+        outcome_2=self.check_win(self.match_up(['0','0','0','0','0','0','0','0','0'], strat, group_1[n]))
+        if outcome_1=="1":
+          score[n]+=1
+        elif outcome_1=="2":
+          score[n]-=1
+        if outcome_2=="2":
+          score[n]+=1
+        elif outcome_2=="1":
+          score[n]-=1
+    return sum(score)/len(score)
+        
+        
+
+  def next_gen(self,strats):
     best=self.tournament(strats)
+    # print(best)
     new_strats=[strats[n] for n in best]
     babies=[]
     for first in range(len(new_strats)):
@@ -35,11 +88,15 @@ class TikTacToe:
         if first!=second:
           babies.append(self.breed(new_strats[first],new_strats[second]))
           #add babies to original then blast
+    for baby in babies:
+      new_strats.append(baby)
+    return new_strats
+    
 
 
   def breed(self,strat_1,strat_2):
     child={}
-    for state in strat_1.key():
+    for state in strat_1.keys():
       option=[strat_1[state],strat_2[state]]
       child[state]=random.choice(option)
     return child
@@ -49,7 +106,7 @@ class TikTacToe:
     score=[0 for n in range(len(strats))]
     size=len(strats)
     for first in range(len(strats)):
-      print(first)
+      # print(first)
       for second in range(len(strats)):
         if first!=second:
           winner=self.check_win(self.match_up(['0','0','0','0','0','0','0','0','0'], strats[first], strats[second]))
@@ -95,18 +152,30 @@ class TikTacToe:
     return pos_states
           
   def check_win(self,state):
-    if state[0]==state[1] and state[1]==state[2] and state[0]!="0":
+    if self.check_same(state, 0, 1, 2):
       return state[0]
-    elif state[3]==state[4] and state[4]==state[5] and state[3]!="0":
+    elif self.check_same(state, 3, 4, 5):
       return state[3]
-    elif state[6]==state[7] and state[7]==state[8] and state[6]!="0":
+    elif self.check_same(state, 6, 7, 8):
       return state[6]
-    elif state[0]==state[4] and state[4]==state[8] and state[0]!="0":
+    elif self.check_same(state, 0, 4, 8):
       return state[0]
-    elif state[2]==state[4] and state[4]==state[6] and state[2]!="0":
+    elif self.check_same(state, 2, 4, 6):
       return state[6]
+    elif self.check_same(state, 0, 3, 6):
+      return state[0]
+    elif self.check_same(state, 1, 4, 7):
+      return state[1]
+    elif self.check_same(state, 2, 5, 8):
+      return state[2]
     elif "0" not in state:
       return "3"
+    else:
+      return False
+
+  def check_same(self,state,uno,dos,tres):
+    if state[uno]==state[dos] and state[dos]==state[tres] and state[uno]!=0:
+      return True
     else:
       return False
 
